556a816db8dbd7fe7ee5d273253b19897352a517
Alex Simmons <alex.simmons.1992@gmail.com>
2018-11-28 10:46:17 +1000

    Initial Commit

---
+++ Satellites_3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Satellites_3
+//
+//  Created by Alex Simmons on 28/11/18.
+//  Copyright © 2018 Alex Simmons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
